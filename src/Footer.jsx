34df333eb0e1014a6f691f408bcca4c21ce372ba
zeki88 <eguimaraz@gmail.com>
2024-11-13 19:53:41 -0300

    fix styles

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+export default function Footer () {
+    return (
+        <div className="footer">
+                <p className="text-white">Copyright©</p>
+                <p className="text-white">2024</p>
+    <a className="github" href="https://github.com/zeki88" target="_blank">
+                <p className="text-white">zeki88</p>
+      <i className="fab fa-github" aria-hidden="true"></i></a>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Footer />
   </StrictMode>,
 )
